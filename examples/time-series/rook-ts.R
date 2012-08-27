dbb74d742d9f3aeed9cb2a2f4d262ea7e7328253
Simon Potter <simon@sjp.co.nz>
2012-08-28 10:05:32 +1200

    Adding feature to load custom datasets.

--- examples/time-series/rook-ts.R
+++ examples/time-series/rook-ts.R
@@ -3,6 +3,19 @@ library(Rook)
 
 source("acf-plots.R")
 
+armaDatasetGen <- function() {
+    dataset <- Nile
+    function(newdata = NULL) {
+        if (is.null(newdata)) {
+            dataset
+        } else {
+            newts <- ts(newdata, start = c(1998, 9), frequency = 12)
+            dataset <<- newts
+        }
+    }
+}
+armaDataset <- armaDatasetGen()
+
 coordsInfoGen <- function() { 
     coords <- NULL
     function(newCoords = NULL) { 
