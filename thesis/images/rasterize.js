7ad00d18808703100ae40f22ba1110fddcdb9659
Simon Potter <simon@sjp.co.nz>
2013-06-13 23:07:18 +1200

    Final version of thesis.

--- thesis/images/rasterize.js
+++ thesis/images/rasterize.js
@@ -9,15 +9,14 @@ if (system.args.length < 3 || system.args.length > 5) {
 } else {
     address = system.args[1];
     output = system.args[2];
-    page.viewportSize = { width: 600, height: 600 };
     if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
         size = system.args[3].split('*');
-        page.paperSize = size.length === 2 ? { width: size[0], height: size[1], margin: '0px' }
-                                           : { format: system.args[3], orientation: 'portrait', margin: '1cm' };
+        page.paperSize = size.length === 2 ? { width: size[0] + "in", height: size[1] + "in", margin: '0cm' }
+                                           : { format: system.args[3], orientation: 'portrait', margin: '0cm' };
     }
-    if (system.args.length > 4) {
-        page.zoomFactor = system.args[4];
-    }
+    // For some reason the rendering is 0.8 times the real size.
+    // Scale by the inverse.
+    page.zoomFactor = 1.25;
     page.open(address, function (status) {
         if (status !== 'success') {
             console.log('Unable to load the address!');
--- thesis/knitr-settings.R
+++ thesis/knitr-settings.R
@@ -1,8 +1,9 @@
 opts_chunk$set(tidy = FALSE, highlight = FALSE, comment = NA,
-               prompt = TRUE, fig.align = "center",
-               fig.width = 5, fig.height = 5,
-               out.width = paste0(5, "in"),
-               out.height = paste0(5, "in"))
+               prompt = TRUE, fig.align = "center", fig.pos = "H",
+               fig.scap = NA, # Do not want short caption
+               fig.width = 3, fig.height = 3,
+               out.width = paste0(3, "in"),
+               out.height = paste0(3, "in"))
 knit_hooks$set(pdfcrop = hook_pdfcrop)
 #knit_theme$set("print")
 
@@ -13,8 +14,12 @@ local({
   options(repos=r)
 })
 
+# Loading frequently used packages
 library(gridSVG)
 library(ggplot2)
+# Apply local lattice fix
+library(lattice, lib.loc = .libPaths()[1])
+
 options(prompt = "R> ", continue = "R+ ", width = 68)
 
 jsLine <- function(line, prefix = "JS> ") {
@@ -33,9 +38,9 @@ printNoEval <- function(text) {
 # Create a line trimming function.
 # Useful in situations like SVG output where the line length
 # is going to be much longer than the page width.
-lineTrim <- function(line, lwd = getOption("width")) {
+lineTrim <- function(line, lwd = getOption("width"), side = "both") {
   library(stringr)
-  line <- str_trim(line)
+  line <- str_trim(line, side = side)
   if (nchar(line) > lwd)
     paste0(substring(line, 1, lwd - 3), "...")
   else
@@ -46,3 +51,51 @@ printedLineTrim <- function(objToPrint, lwd = getOption("width")) {
     lines <- capture.output(print(objToPrint))
     paste0(sapply(lines, lineTrim, lwd), collapse = "\n")
 }
+
+hook_phantom <- function(before, options, envir) {
+    suppressPackageStartupMessages(library(gridSVG))
+    if (before)
+        return()
+
+    ext <- tolower(options$fig.ext)
+    if (! any(ext %in% c('pdf', 'png'))) {
+        warning('this hook only exports to pdf and png at the moment')
+        return()
+    }
+
+    # Generate filenames
+    svg_name <- paste(fig_path("", options), ".svg", sep = "")
+    out_name <- paste(fig_path("", options), ".", ext, sep = "")
+
+    # Run gridSVG, optional arguments in gridSVG_args list if required
+    if (! is.null(options$gridSVG_args)) {
+        args <- formals(grid.export)
+        # Set defaults
+        args$name <- svg_name
+        for (arg in names(gridSVG_args))
+            args[[arg]] <- gridSVG_args[[arg]]
+        do.call("grid.export", args)
+    } else {
+        grid.export(svg_name)
+    }
+
+    if (! nzchar(Sys.which('phantomjs'))) {
+        warning('cannot find phantomjs; please install and put it in PATH')
+        return()
+    }
+
+    message("converting ", svg_name)
+    cmd <- paste("phantomjs",
+                 "images/rasterize.js",
+                 shQuote(svg_name),
+                 shQuote(out_name),
+                 paste0(options$fig.width, "*", options$fig.height))
+    if (.Platform$OS.type == 'windows')
+        cmd <- paste(Sys.getenv("COMSPEC"), "/c", cmd)
+    system(cmd)
+
+    # No longer need the source image
+    unlink(svg_name)
+}
+
+knit_hooks$set(phantom = hook_phantom)
