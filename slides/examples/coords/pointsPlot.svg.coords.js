eecbb3e4bcc72f4c744a85cfa19d134789d8c1ea
Simon Potter <simon@sjp.co.nz>
2013-06-25 12:10:44 +1200

    Add seminar slides so far.

---
+++ slides/examples/coords/pointsPlot.svg.coords.js
@@ -0,0 +1,20 @@
+var gridSVGCoords = {
+ "ROOT": {
+ "x":      0,
+"y":      0,
+"width":    504,
+"height":    504,
+"xscale": [      0,    504 ],
+"yscale": [      0,    504 ],
+"inch":     72 
+},
+"panelvp.1": {
+ "x":   50.4,
+"y":   50.4,
+"width":  403.2,
+"height":  403.2,
+"xscale": [      0,     20 ],
+"yscale": [      0,     20 ],
+"inch":     72 
+} 
+};
---
+++ slides/slide_config.js
@@ -0,0 +1,31 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'A Piece of the Web Graphics Puzzle',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Simon Potter',
+    company: '<br>Department of Statistics<br>University of Auckland',
+    email: 'simon.potter@auckland.ac.nz'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
